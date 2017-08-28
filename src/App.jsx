310d6b1370ec4e687b4426723ca0824a2ce024fa
Alex Smith <alexsmith716@outlook.com>
2017-08-28 11:31:24 -0400

    Basic webpack.config working test jsx using loader babel-loader.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <h1>Header!!</h1>
         <div className="container-fluid">
           <hr />
           <h5><small>
